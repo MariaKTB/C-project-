60b73f231d6ec1136dd0534bbaf06385574c5d4a
MariaKTB <98776869+MariaKTB@users.noreply.github.com>
2024-02-29 17:33:24 +0100

    Add files via upload

---
+++ MyGame/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    internal class Player : GameObject
+    {
+        public void char_move()
+        {
+
+        }
+        public void char_render()
+        {
+
+        }
+        public void shootBullet()
+        {
+
+        }
+    }
+}
---
+++ MyGame/Zombie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    internal class Zombie : GameObject
+    {
+        public void zomb_move()
+        {
+
+        }
+        public void zomb_render()
+        {
+
+        }
+
+    }
+}
